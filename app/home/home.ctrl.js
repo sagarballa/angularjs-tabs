9c50debf9cfa4f09cf91a3cb01cbe225045f8fa0
Sagar Balla <sagar.balla@gmail.com>
2016-11-20 10:44:27 +0530

    Added Tabs

--- app/home/home.ctrl.js
+++ app/home/home.ctrl.js
@@ -5,6 +5,26 @@
     
     function homeController(){
         var vm = this;
-       
+        vm.tabs = [];
+        
+        vm.addTab = addTab;
+        vm.removeTab = removeTab;
+        
+        function addTab() {
+            var tab = {
+                view : "test"
+            }
+            vm.tabs.push(tab);
+        }
+        
+        function removeTab(index) {
+            vm.tabs.slice(index,1);
+        }
+        
+        function init(){
+            addTab();
+        }
+        
+        init();
     }
 })();
